1d43de67c36db77d1af3012e130c3c576a92e669
Bharat Kumar <kumarbharat661@gmail.com>
2021-08-16 09:23:37 +0530

    installed styled components and created styles for showcard

--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ShowCard from './ShowCard'
+import {FlexGrid} from '../../styled'
 import IMAGE_NOT_FOUND from '../../images/not-found.png'
 
 const ShowGrid = ({data}) => {
     return (
-        <div>
+        <FlexGrid>
             {data.map( ({show}) => <ShowCard key={show.id} id={show.id} name={show.name} image={show.image ? show.image.medium : IMAGE_NOT_FOUND} summary={show.summary}/>)}
-        </div>
+        </FlexGrid>
     )
 }
 
