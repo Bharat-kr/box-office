65530861800e770ab41778f39ce834aeaed890bd
Bharat Kumar <kumarbharat661@gmail.com>
2021-08-19 16:28:08 +0530

    replaced useState with useReducer on show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,13 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer } from "react";
 import { useParams } from "react-router";
 import { apiGet } from "../misc/config";
+
+const reducer = (prevState , action) => {
+    switch (action.type) {
+        case 'FETCH_SUCCESS':{
+            return {...prevState , isLoading: false ,show: action.show}
+        }
+        case 'FETCH_FAILED':{
+            return {...prevState , isLoading: false , error: action.error}
+        }
+
+    
+        default:
+            return prevState;
+    }
+}
+
+const initialState = {
+    show: null ,
+    isLoading: true,
+    error: null,
+}
 
 const Show = () => {
     const { id } = useParams();
 
-    const [show, setShow] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const [{show , isLoading , error} , dispatch] = useReducer(reducer, initialState);
+
+    // const [show, setShow] = useState(null);
+    // const [isLoading, setIsLoading] = useState(true);
+    // const [error, setError] = useState(null);
 
     useEffect(() => {
         let isMounted = true;
@@ -15,15 +38,17 @@ const Show = () => {
             .then((results) => {
                 setTimeout(() => {
                     if(isMounted){
-                        setShow(results);
-                        setIsLoading(false);
+                        dispatch( {type: 'FETCH_SUCCESS' , show: results})
+                        // setShow(results);
+                        // setIsLoading(false);
                     }
                 }, 2000);
             })
             .catch((err) => {
                 if(isMounted){
-                    setError(err.message);
-                    setIsLoading(false);
+                    dispatch( {type: 'FETCH_FAILED' , error : err.message})
+                    // setError(err.message);
+                    // setIsLoading(false);
                 }
             });
 
