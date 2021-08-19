7566d5fd90196ae3ee6aaa8fead49352cf91b23f
Bharat Kumar <kumarbharat661@gmail.com>
2021-08-19 23:24:57 +0530

    using custom hooks for starred shows

--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -1,14 +1,38 @@
-import React from 'react'
-import ShowCard from './ShowCard'
-import {FlexGrid} from '../../styled'
-import IMAGE_NOT_FOUND from '../../images/not-found.png'
+import React from "react";
+import ShowCard from "./ShowCard";
+import { FlexGrid } from "../../styled";
+import IMAGE_NOT_FOUND from "../../images/not-found.png";
+import { useShows } from "../../misc/custom-hooks";
 
-const ShowGrid = ({data}) => {
+const ShowGrid = ({ data }) => {
+    const [starredShows, dispatchedStarred] = useShows();
     return (
         <FlexGrid>
-            {data.map( ({show}) => <ShowCard key={show.id} id={show.id} name={show.name} image={show.image ? show.image.medium : IMAGE_NOT_FOUND} summary={show.summary}/>)}
-        </FlexGrid>
-    )
-}
+            {data.map(({ show }) => {
+                const isStarred = starredShows.includes(show.id);
 
-export default ShowGrid
+                const onStarClick = () => {
+                    if(isStarred){
+                        dispatchedStarred({ type:'REMOVE', showId: show.id})
+                    }else{
+                        dispatchedStarred({type:'ADD', showId: show.id})
+                    }
+                }
+
+                return (
+                    <ShowCard
+                        key={show.id}
+                        id={show.id}
+                        name={show.name}
+                        image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
+                        summary={show.summary}
+                        onStarClick={onStarClick}
+                        isStarred={isStarred}
+                    />
+                );
+            })}
+        </FlexGrid>
+    );
+};
+
+export default ShowGrid;
--- src/components/show/ShowMainData.js
+++ src/components/show/ShowMainData.js
@@ -1,34 +1,39 @@
-import React from 'react';
-import {Star} from '../../styled';
-import {MainDataWrapper , Headline , TagList} from './ShowMainData.styled'
-import IMG_PLACEHOLDER from '../../images/not-found.png';
+import React from "react";
+import { Star } from "../../styled";
+import { MainDataWrapper, Headline, TagList } from "./ShowMainData.styled";
+import IMG_PLACEHOLDER from "../../images/not-found.png";
 
 const ShowMainData = ({ name, rating, summary, tags, image }) => {
     return (
-      <MainDataWrapper>
-        <img src={image ? image.original : IMG_PLACEHOLDER} alt="show-cover" />
-        <div className="text-side">
-          <Headline>
-            <h1>{name}</h1>
-            <div>
-              <Star />
-              <span>{rating.average || 'N/A'}</span>
-            </div>
-          </Headline>
-          <div className="summary" dangerouslySetInnerHTML={{ __html: summary }} />
-  
-          <div>
-            Tags:{' '}
-            <TagList>
-              {tags.map((tag, i) => (
-                <span key={i}>{tag}</span>
-              ))}
-            </TagList>
-          </div>
-        </div>
-      </MainDataWrapper>
-    );
-  };
-  
+        <MainDataWrapper>
+            <img
+                src={image ? image.original : IMG_PLACEHOLDER}
+                alt='show-cover'
+            />
+            <div className='text-side'>
+                <Headline>
+                    <h1>{name}</h1>
+                    <div>
+                        <Star active />
+                        <span>{rating.average || "N/A"}</span>
+                    </div>
+                </Headline>
+                <div
+                    className='summary'
+                    dangerouslySetInnerHTML={{ __html: summary }}
+                />
 
-export default ShowMainData
+                <div>
+                    Tags:{" "}
+                    <TagList>
+                        {tags.map((tag, i) => (
+                            <span key={i}>{tag}</span>
+                        ))}
+                    </TagList>
+                </div>
+            </div>
+        </MainDataWrapper>
+    );
+};
+
+export default ShowMainData;
