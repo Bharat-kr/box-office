7b48c39080b2b8c36e03b01ed11960b26b5ae0ab
Bharat Kumar <kumarbharat661@gmail.com>
2021-08-19 22:58:47 +0530

    created custom hook for shows

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,67 +1,96 @@
-import React, { useState , useEffect } from 'react';
-import ActorGrid from '../components/actor/ActorGrid';
-import MainPageLayout from '../components/MainPageLayout';
-import ShowGrid from '../components/show/ShowGrid';
-import {apiGet} from '../misc/config';
+import React, { useState, useEffect } from "react";
+import ActorGrid from "../components/actor/ActorGrid";
+import MainPageLayout from "../components/MainPageLayout";
+import ShowGrid from "../components/show/ShowGrid";
+import { apiGet } from "../misc/config";
 
 const Home = () => {
-    const [input , setInput] = useState('');
-    const [results , setResults] = useState(null);
-    const [searchOption , setSearchOption] = useState('shows');
+    const [input, setInput] = useState("");
+    const [results, setResults] = useState(null);
+    const [searchOption, setSearchOption] = useState("shows");
 
-    const isShowsSearch = searchOption === 'shows';
+    const isShowsSearch = searchOption === "shows";
 
-    useEffect(()=>{console.log('use effect is run')}, [])
+    useEffect(() => {
+        console.log("use effect is run");
+    }, []);
 
-    const onInputChange = ev => {
+    const onInputChange = (ev) => {
         setInput(ev.target.value);
-    }
+    };
 
-    const onSearch = () =>{
+    const onSearch = () => {
         // https://api.tvmaze.com/search/shows?q=girls
-        apiGet(`/search/${searchOption}?q=${input}`).then(data => {setResults(data); console.log(data)});
-    }
+        apiGet(`/search/${searchOption}?q=${input}`).then((data) => {
+            setResults(data);
+            console.log(data);
+        });
+    };
 
     const onKeyDown = (ev) => {
-        if(ev.keyCode === 13){
-           onSearch();
+        if (ev.keyCode === 13) {
+            onSearch();
         }
-    }
+    };
 
     const onRadioChange = (ev) => {
         setSearchOption(ev.target.value);
-    }
+    };
     console.log(searchOption);
 
     const renderResults = () => {
-        if(results && results.length === 0){
-            return <div>No results found</div>
+        if (results && results.length === 0) {
+            return <div>No results found</div>;
         }
 
-        if(results && results.length > 0){
-            return results[0].show ? <ShowGrid data={results} /> : <ActorGrid data={results} />;
+        if (results && results.length > 0) {
+            return results[0].show ? (
+                <ShowGrid data={results} />
+            ) : (
+                <ActorGrid data={results} />
+            );
         }
 
         return null;
-    }
+    };
 
     return (
         <MainPageLayout>
-            <input type="text" placeholder="Search for something" onChange ={onInputChange} onKeyDown={onKeyDown} value={input}/>
+            <input
+                type='text'
+                placeholder='Search for something'
+                onChange={onInputChange}
+                onKeyDown={onKeyDown}
+                value={input}
+            />
             <div>
-                <label htmlFor="shows-search">
+                <label htmlFor='shows-search'>
                     Shows
-                    <input id="shows-search" type="radio" value="shows" checked={isShowsSearch} onChange={onRadioChange}/>
+                    <input
+                        id='shows-search'
+                        type='radio'
+                        value='shows'
+                        checked={isShowsSearch}
+                        onChange={onRadioChange}
+                    />
                 </label>
-                <label htmlFor="actors-search">
+                <label htmlFor='actors-search'>
                     Actors
-                    <input id="actors-search" type="radio" value="people" checked={!isShowsSearch} onChange={onRadioChange}/>
+                    <input
+                        id='actors-search'
+                        type='radio'
+                        value='people'
+                        checked={!isShowsSearch}
+                        onChange={onRadioChange}
+                    />
                 </label>
             </div>
-            <button type="button" onClick={onSearch}>Search</button>
+            <button type='button' onClick={onSearch}>
+                Search
+            </button>
             {renderResults()}
         </MainPageLayout>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,33 +5,35 @@ import ShowMainData from "../components/show/ShowMainData";
 import Seasons from "../components/show/Seasons";
 import Cast from "../components/show/Cast";
 import { apiGet } from "../misc/config";
-import { ShowPageWrapper , InfoBlock } from "./Show.styled";
+import { ShowPageWrapper, InfoBlock } from "./Show.styled";
 
-const reducer = (prevState , action) => {
+const reducer = (prevState, action) => {
     switch (action.type) {
-        case 'FETCH_SUCCESS':{
-            return {...prevState , isLoading: false ,show: action.show}
+        case "FETCH_SUCCESS": {
+            return { ...prevState, isLoading: false, show: action.show };
         }
-        case 'FETCH_FAILED':{
-            return {...prevState , isLoading: false , error: action.error}
+        case "FETCH_FAILED": {
+            return { ...prevState, isLoading: false, error: action.error };
         }
 
-    
         default:
             return prevState;
     }
-}
+};
 
 const initialState = {
-    show: null ,
+    show: null,
     isLoading: true,
     error: null,
-}
+};
 
 const Show = () => {
     const { id } = useParams();
 
-    const [{show , isLoading , error} , dispatch] = useReducer(reducer, initialState);
+    const [{ show, isLoading, error }, dispatch] = useReducer(
+        reducer,
+        initialState
+    );
 
     // const [show, setShow] = useState(null);
     // const [isLoading, setIsLoading] = useState(true);
@@ -41,23 +43,23 @@ const Show = () => {
         let isMounted = true;
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
             .then((results) => {
-                    if(isMounted){
-                        dispatch( {type: 'FETCH_SUCCESS' , show: results})
-                        // setShow(results);
-                        // setIsLoading(false);
-                    }
+                if (isMounted) {
+                    dispatch({ type: "FETCH_SUCCESS", show: results });
+                    // setShow(results);
+                    // setIsLoading(false);
+                }
             })
             .catch((err) => {
-                if(isMounted){
-                    dispatch( {type: 'FETCH_FAILED' , error : err.message})
+                if (isMounted) {
+                    dispatch({ type: "FETCH_FAILED", error: err.message });
                     // setError(err.message);
                     // setIsLoading(false);
                 }
             });
 
-            return () => {
-                isMounted = false;
-            };
+        return () => {
+            isMounted = false;
+        };
     }, [id]);
 
     console.log("show", show);
@@ -70,22 +72,34 @@ const Show = () => {
         return <div>Error occured {error}</div>;
     }
 
-    return <ShowPageWrapper>
-        <ShowMainData image={show.image} name={show.name} rating={show.rating} summary={show.summary} tags={show.genres}/>
+    return (
+        <ShowPageWrapper>
+            <ShowMainData
+                image={show.image}
+                name={show.name}
+                rating={show.rating}
+                summary={show.summary}
+                tags={show.genres}
+            />
 
-        <InfoBlock>
-            <h2>Details</h2>
-            <Details status={show.status} network={show.network} premiered={show.premiered} />
-        </InfoBlock>
-        <InfoBlock>
-            <h2>Seasons</h2>
-            <Seasons seasons={show._embedded.seasons} />
-        </InfoBlock>
-        <InfoBlock>
-            <h2>Cast</h2>
-            <Cast cast={show._embedded.cast}/>
-        </InfoBlock>
-    </ShowPageWrapper>;
+            <InfoBlock>
+                <h2>Details</h2>
+                <Details
+                    status={show.status}
+                    network={show.network}
+                    premiered={show.premiered}
+                />
+            </InfoBlock>
+            <InfoBlock>
+                <h2>Seasons</h2>
+                <Seasons seasons={show._embedded.seasons} />
+            </InfoBlock>
+            <InfoBlock>
+                <h2>Cast</h2>
+                <Cast cast={show._embedded.cast} />
+            </InfoBlock>
+        </ShowPageWrapper>
+    );
 };
 
 export default Show;
